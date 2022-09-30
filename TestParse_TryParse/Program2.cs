142bef1df1c34151b95ac5e5603f0c5595d562b5
Turan98 <48137040+Turan98@users.noreply.github.com>
2022-09-30 02:24:57 -0700

    test3

---
+++ TestParse_TryParse/Program2.cs
@@ -0,0 +1,7 @@
+﻿// Program about parse and tryparse
+string str = "5.6";
+double a = double.Parse(str);
+double b = Convert.ToInt32(Console.ReadLine());
+double c;
+ c = a + b;
+Console.WriteLine(c);
